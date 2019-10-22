b30c4028c5a2b4bd7cdc260a068904906bd9a9eb
IT <IT@IT-PC>
2019-10-22 16:14:47 +0900

    g

---
+++ src/exception/PwdFormatTest.java
@@ -0,0 +1,49 @@
+package exception;
+
+public class PwdFormatTest {
+	private String pass;
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass)throws IDFormatException {
+		if(pass == null) {
+			throw new IDFormatException("비밀번호는 null일 수 없습니다.");
+		}
+		else if(pass.length() <= 5) {
+			throw new IDFormatException("비밀번호는 5자 이상이어야 합니다.");
+		}
+		else if(pass.matches("[a-zA-Z]+")) {
+			throw new IDFormatException("비밀번호는 문자열로만 이루어질 수 없습니다.");
+		}
+		this.pass = pass;
+	}
+
+	public static void main(String[] args) {
+		PwdFormatTest test = new PwdFormatTest();
+		
+		String pass = null;
+		try {
+			test.setPass(pass);
+		} catch (IDFormatException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		pass = "abcd";
+		try {
+			test.setPass(pass);
+		} catch (IDFormatException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		pass = "abcdefg";
+		try {
+			test.setPass(pass);
+		} catch (IDFormatException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
